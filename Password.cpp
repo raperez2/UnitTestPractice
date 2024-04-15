f219aba7a39fa928d504d643e3b73ce88f14bb9d
raperez2 <70178601+raperez2@users.noreply.github.com>
2024-04-15 12:10:12 -0700

    Added mixed_case implamentation
    Added cctype library to check for at least one upper and lower case

--- Password.cpp
+++ Password.cpp
@@ -18,3 +18,21 @@ int Password::count_leading_characters(string phrase){
   }
   return repetition;
 }
+
+/*
+  The function receives a string, checks for at least one Uppercase letter and a single Lowercase letter.
+  and any ASCII characters are allowed.
+*/
+bool Password::has_mixed_case(string phrase){
+  bool upper, lower = false;
+  int index = 0;
+  while( index < phrase.length()-1){
+    if(isupper(phrase[index])){
+      upper = true;
+    }else if(islower(phrase[index])){
+      lower = true;
+    }
+    index++;
+  }
+  return (upper && lower);
+}
