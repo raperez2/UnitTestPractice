d9b3dc8b7cba2b85de733c8f6ac7eb3393fcb328
raperez2 <70178601+raperez2@users.noreply.github.com>
2024-04-24 11:50:29 -0700

    Added set and auth
    Added implementation of set and auth

--- Password.cpp
+++ Password.cpp
@@ -1,7 +1,17 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
+#include <vector>
 
 using std::string;
+using std::vector;
+
+/*
+  Sets the first password in the list to "ChicoCA-95929"
+*/
+Password::Password(){
+  pass_history.push_back("ChicoCA-95929");
+}
 
 /*
   The function receives a string counts how many times the same character 
@@ -19,12 +29,10 @@ int Password::count_leading_characters(string phrase){
   return repetition;
 }
 
-/*
-  The function receives a string, checks for at least one Uppercase letter and a single Lowercase letter.
-  and any ASCII characters are allowed.
-*/
+//The function receives a string, checks for at least one Uppercase letter and a single Lowercase letter.
+//and any ASCII characters are allowed.
 bool Password::has_mixed_case(string phrase){
-  bool upper, lower = false;
+  bool upper = false, lower = false;
   int index = 0;
   while( index < phrase.length()-1){
     if(isupper(phrase[index])){
@@ -35,4 +43,24 @@ bool Password::has_mixed_case(string phrase){
     index++;
   }
   return (upper && lower);
+}
+
+void Password::set(string phrase){
+  if(phrase.length() >=8 && phrase.length() <=20){
+    if(has_mixed_case(phrase) && count_leading_characters(phrase) <= 3){
+      int index = sizeof(pass_history);
+      if(pass_history[index-1] != phrase){
+        pass_history.push_back(phrase);
+      }
+    }
+  }
+}
+
+bool Password::authenticate(string phrase){
+  bool result = false;
+  int index = sizeof(pass_history);
+  if(phrase == pass_history.back()){
+    result = true;
+  }
+  return result;
 }
